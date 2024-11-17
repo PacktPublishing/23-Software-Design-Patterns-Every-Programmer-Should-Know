34667ffdf3ad82aaf11266bdc95ba9efe7c30a7e
Ferdinand Redelinghuys <fcredelinghuys@gmail.com>
2024-11-17 23:05:33 +0000

    singleton: thread safety testing

--- chp01_Singleton/TypeScript/Logger_thread_safe.js
+++ chp01_Singleton/TypeScript/Logger_thread_safe.js
@@ -1,3 +1,4 @@
+// might need to run: tsc --target es6 Logger_thread_safe.ts
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
